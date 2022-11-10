da442967d4641b4cb028e80426ce752f9692de11
Developer <techconquerorsv2@gmail.com>
2022-11-10 14:04:36 +0800

    Homepage styling and Navigation Revamp

--- assets/js/components/navigation.js
+++ assets/js/components/navigation.js
@@ -1,27 +1,23 @@
 export const navigation = () => {
   const navMarker = document.querySelector('.nav-marker');
-  const navContainer = document.querySelector('.nav');
-  const mobileNavToggle = document.querySelector('.nav-toggle');
   const navItemContainer = document.querySelectorAll('.nav-item');
 
-  mobileNavToggle.addEventListener('click', (e) => {
-    e.target.classList.toggle('active');
-    navContainer.classList.toggle('active');
-    document.querySelector('section').style.overflow = 'hidden'
-  });
-
-  navItemContainer.forEach((list) => {
-    const navItemLink = list.querySelector('a');
-
-    navItemLink.addEventListener('mouseover', () => {
-      let position = list.getBoundingClientRect();
-      navMarker.classList.add('active');
-      navMarker.style.left = position.x + 'px';
-      navMarker.style.width = position.width + 'px';
+  if (window.innerWidth >= 915) {
+    navItemContainer.forEach((list) => {
+      const navItemLink = list.querySelector('a');
+  
+      navItemLink.addEventListener('mouseover', (e) => {
+        let position = list.getBoundingClientRect();
+        e.target.parentNode.classList.add('active');
+        navMarker.classList.add('active');
+        navMarker.style.left = position.x + 'px';
+        navMarker.style.width = position.width + 'px';
+      });
+  
+      list.addEventListener('mouseout', (e) => {
+        list.classList.remove('active');
+        navMarker.classList.remove('active');
+      });
     });
-
-    list.addEventListener('mouseout', () => {
-      navMarker.classList.remove('active');
-    });
-  });
+  }
 };
