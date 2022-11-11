9fb14ed968400bb101d978d0e70361b46268306d
Developer <techconquerorsv2@gmail.com>
2022-11-12 02:52:08 +0800

    Culture Page

---
+++ assets/js/components/read-more.js
@@ -0,0 +1,25 @@
+export const readMore = () => {
+  const readMoreBtn = document.querySelectorAll('.read-more-toggle');
+
+  readMoreBtn.forEach((btn) => {
+    btn.addEventListener('click', function(e) {
+      e.preventDefault();
+      const readMoreId = e.target.getAttribute('data-read-more-id');
+      const dots = document.querySelector(`.dots[data-read-more-id="${readMoreId}"]`);
+      const hiddenContent = document.querySelector(`.read-more-content[data-read-more-id="${readMoreId}"]`);
+
+      [dots, hiddenContent].forEach(item => {
+        item.classList.toggle('hide')
+      });
+
+      e.target.classList.toggle('shown');
+
+
+      if (e.target.classList.contains('shown')) {
+        e.target.textContent = 'Read Less';
+      } else {
+        e.target.textContent = 'Read More';
+      }
+    });
+  });
+}
